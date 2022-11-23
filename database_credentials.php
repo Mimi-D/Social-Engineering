132541275c398eff4385f7ff816ddc01473c89f8
Mimi-D <67998464+Mimi-D@users.noreply.github.com>
2022-11-23 23:08:14 +0000

    Update database_credentials.php

--- database_credentials.php
+++ database_credentials.php
@@ -1,6 +1,6 @@
 <?php
-    define('servername','localhost');
-    define('username','root');
-    define('password','');
-    define('dbname','login_se');
+    define('servername','us-cdbr-east-06.cleardb.net');
+    define('username','bee9c0265e3c28');
+    define('password','42ef55a5');
+    define('dbname','heroku_261862934fdc4d4');
 ?>
